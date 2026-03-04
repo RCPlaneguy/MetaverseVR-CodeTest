51871ca43213fbaba238aa02bce7ef740440f0fd
RCPlaneguy <louistorrestailfer@gmail.com>
2026-03-04 23:10:52 +0000

    improved movement of powerboat, cleaner implementation of force based movement and steering

--- Assets/Scripts/Player/Powerboat.cs
+++ Assets/Scripts/Player/Powerboat.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
@@ -8,94 +6,109 @@ using UnityEngine.Rendering.Universal;
 public class Powerboat : MonoBehaviour
 {
     [Header("Helm")]
-    [SerializeField] private float maxAcceleration;
-    [SerializeField] private float turnSensitivty = 1f;
-    [SerializeField] private float maxSteerAngle = 30f;
-    [SerializeField] private float minSteerageSpeed = 10f;
+    [SerializeField] private float maxSpeed = 30f;
+    [SerializeField] private float maxSpeedReverse = 15f;
+    [SerializeField] private float turningSpeed = 10f;
 
     [Header("Elements")]
     [SerializeField] private Rigidbody rb;
     public Rigidbody Rigidbody => rb;
     [SerializeField] private Transform engines;
+    [SerializeField] private float maxEngineAngle = 30f;
 
     [Header("Visual Effects")]
     [SerializeField] private Volume globalVolume;
     [SerializeField] private float maxVignetteIntensity = 0.5f;
-    [SerializeField] private float maxVignetteSpeed = 30f;
 
     [Header("Debug")]
+    [SerializeField] private Vector2 inputs = new();
+    [SerializeField] private float forceMagnitude;
     [SerializeField] private float currentSpeed;
-    [SerializeField] private Vector3 directionRot;
-    [SerializeField] private float inputX;
-    [SerializeField] private float inputY;
-    [SerializeField] private Vector3 engineRot;
-    [SerializeField] private Vector3 force;
 
-    private const float SteeringSpeed = 0.6f;
-    private const float ForceMultiplier = 1200f;
+    [Space]
+    [SerializeField] private float torqueMagnitude;
+    [SerializeField] private float currentTurningSpeed;
+
+    private Vector3 engineRot;
     private Vignette _vignette;
     private bool _vignetteFound;
+
+    // divide the speed values by this to get the 
+    // force value necessary to achieve the corresponding top speed
+    private const float speedToForce = 4.5f;
 
     private void Start()
     {
         engineRot = engines.localEulerAngles;
-        directionRot = transform.eulerAngles;
         _vignetteFound = globalVolume.profile.TryGet(out _vignette);
         if (!_vignetteFound)
             Debug.LogError("No vignette found!");
-
     }
 
     private void Update()
     {
         if (GameManager.GamePaused)
             return;
-        HandlePhysicsMovement();
 
-        float ratio = currentSpeed / maxVignetteSpeed;
-        _vignette.intensity.value = maxVignetteIntensity * ratio;
-
+        HandleMovement();
+        HandleVisuals();
     }
 
-    private void HandlePhysicsMovement()
+    private void HandleMovement()
     {
-        // accelerate boat in direction it's facing, with impulse force scaled based on inputY
-        force = ForceMultiplier * inputY * maxAcceleration * Time.deltaTime * transform.forward;
+        // calculate force magnitude using different force values for forward and reverse
+        forceMagnitude = inputs.y > 0
+            ? ToForce(maxSpeed) * inputs.y
+            : ToForce(maxSpeedReverse) * inputs.y;
 
-        // apply impulse force
-        rb.AddForce(force, ForceMode.Force);
+        // apply force to rb along forward axis
+        rb.AddForce(forceMagnitude * transform.forward);
 
-        // angle engines based on inputX,
-        float steerAngle = -inputX * turnSensitivty * maxSteerAngle * 2;
-        engineRot.y = Mathf.LerpAngle(engines.localEulerAngles.y, steerAngle, SteeringSpeed);
+        // calculate torque magnitude
+        torqueMagnitude = turningSpeed * inputs.x;
+
+        // if reversing, reverse direction of turn
+        if (inputs.y < 0)
+            torqueMagnitude *= -1f;
+
+        // apply torque to rb along vertical axis
+        rb.AddTorque(torqueMagnitude * transform.up);
+
+        // update values for use elsewhere and for debug
+        currentSpeed = rb.velocity.magnitude;
+        currentTurningSpeed = rb.angularVelocity.magnitude;
+    }
+
+    private void HandleVisuals()
+    {
+        // angle engines based on inputx
+        engineRot.y = -inputs.x * maxEngineAngle;
         engines.localEulerAngles = engineRot;
 
-        currentSpeed = rb.velocity.magnitude;
-
-        // if engines aren't running, don't steer
-        if (force.magnitude < minSteerageSpeed)
-            return;
-
-        // determine turn increment based on current steerangle, invert it if going forward
-        float forwardInvert = inputY > 0 ? -1 : 1;
-        float increment = forwardInvert * steerAngle * Time.deltaTime;
-
-        // increment boat direction by this value
-        Vector3 tempRot = transform.eulerAngles;
-        tempRot.y += increment;
-        transform.eulerAngles = tempRot;
+        // handle speed vignette
+        float ratio = currentSpeed / maxSpeed;
+        if (ratio > 1)
+            ratio = 1;
+        _vignette.intensity.value = maxVignetteIntensity * ratio;
     }
 
     public void OnMove(InputValue input)
     {
-        Vector2 inputVector = input.Get<Vector2>();
-        inputX = inputVector.x;
-        inputY = inputVector.y;
+        inputs = input.Get<Vector2>();
     }
 
     private void OnDrawGizmos()
     {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position + (Vector3.up * 2), rb.velocity);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position + (Vector3.up * 2), forceMagnitude * transform.forward);
+
+
         Gizmos.color = Color.green;
-        Gizmos.DrawRay(engines.position, force);
+        Gizmos.DrawRay(transform.position + (Vector3.up * 2), torqueMagnitude * transform.up);
     }
+
+    private float ToForce(float speed) => speed / speedToForce;
 }
