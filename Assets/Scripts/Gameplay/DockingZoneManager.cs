e0f798099f6b5c80ae4c83e8ceac2e6432e0dc4f
RCPlaneguy <louistorrestailfer@gmail.com>
2026-03-05 23:27:29 +0000

    cleaning up scene, improving docking logic

--- Assets/Scripts/Gameplay/DockingZoneManager.cs
+++ Assets/Scripts/Gameplay/DockingZoneManager.cs
@@ -30,15 +30,27 @@ public class DockingZoneManager : MonoBehaviour
         if (!_isDocking)
             return;
 
+        string reason = string.Empty;
+
         _timeRemaining -= Time.deltaTime;
 
-        if (playerBoat.forceMagnitude > maxDockingForce)
+        if (Mathf.Abs(playerBoat.forceMagnitude) > maxDockingForce)
+        {
             _timeRemaining = dockingTime;
+            reason = "\nMoving too much";
+        }
 
         if (!IsBoatDockedPortside())
+        {
             _timeRemaining = dockingTime;
+            if (!string.Equals(reason, string.Empty))
+                reason += " + ";
+            reason += "\nNot docked portside";
+        }
+        if (string.Equals(reason, string.Empty))
+            reason = "\nGood to go!";
 
-        dockingTimer.text = $"Docking: {_timeRemaining:0.0}s remaining";
+        dockingTimer.text = $"Docking: {_timeRemaining:0.0}s remaining: {reason}";
 
         if (_timeRemaining <= 0f)
         {
@@ -52,16 +64,16 @@ public class DockingZoneManager : MonoBehaviour
     {
         Vector3 starboard = playerBoat.transform.right;
         Vector3 ahead = playerBoat.transform.forward;
-        Vector3 toDock = Vector3.Normalize(cleat.position - playerBoat.transform.position);
+        Vector3 portwards = cleat.forward;
 
-        // if more to starboard, it's not docked portside
-        starboardDot = Vector3.Dot(starboard, toDock);
-        if (starboardDot > 0)
+        // if above -0.8f, it's angled too far away from portside
+        starboardDot = Vector3.Dot(starboard, portwards);
+        if (starboardDot > -0.8f)
             return false;
 
         // checking that it's not quite fully ahead or 
         // astern to make sure it's not pointing into dock
-        aheadDot = Vector3.Dot(ahead, toDock);
+        aheadDot = Vector3.Dot(ahead, portwards);
         return Mathf.Abs(aheadDot) < 0.75f;
     }
 
